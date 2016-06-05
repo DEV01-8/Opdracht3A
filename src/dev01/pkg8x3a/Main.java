da50a1330bf90930ab260d52c47ad2d6c2759b19
Johan Bos <bosbosjohan@gmail.com>
2016-06-05 10:58:50 +0200

    Update
    - Added G4P Library
    - Added Start/Stop Button
    - Changed ellipse() to rect()

--- src/dev01/pkg8x3a/Main.java
+++ src/dev01/pkg8x3a/Main.java
@@ -5,7 +5,13 @@
  */
 package dev01.pkg8x3a;
 
+import g4p_controls.G4P;
+import g4p_controls.GButton;
+import g4p_controls.GCScheme;
+import g4p_controls.GEvent;
+import g4p_controls.GLabel;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import processing.core.PApplet;
@@ -26,29 +32,40 @@ public class Main extends PApplet {
     private static final float minY = startY - 1000f;
     private static final float maxY = startY + 1000f;
     final static Logger logger = Logger.getLogger(Main.class);
-
+    boolean paused = false;
+    GButton startButton;
+    GButton stopButton;
+    
     @Override
     public void setup() {
         background(255);
         surface.setTitle("Waterhoogte Rotterdam Oost");
+
+        G4P.setGlobalColorScheme(GCScheme.GREEN_SCHEME);
+
+        startButton = new GButton(this, 30, 30, 200, 30, "Start");
+        stopButton = new GButton(this, 250, 30, 200, 30, "Stop");
+        startButton.addEventHandler(this, "handleStartButtonEvents");
+        stopButton.addEventHandler(this, "handleStopButtonEvents");
     }
 
     @Override
     public void settings() {
         size(1000, 680);
+
     }
 
     public static void main(String[] args) {
         BasicConfigurator.configure();
-
         try {
+            PApplet.main(new String[]{Main.class.getName()});
             results = parseCSV.read();  //Get all items from parseCSV
         } catch (Exception e) {
             logger.info(e);
         }
-        PApplet.main(new String[]{Main.class.getName()});
     }
 
+    //Method to map xyz coordinates
     private void startMap(float x, float y, float z) {
         try {
             float minZ = parseCSV.minZ;     //min value of Z ~ -16
@@ -58,46 +75,67 @@ public class Main extends PApplet {
             float mapY = map(y, maxY, minY, 0, height);  //map y
             float mapZ = map(z, minZ, maxZ, 0, 216);     //map z
 
-            /*
-            if(mapZ > -16f && mapZ < 3f){
-                stroke(color(mapZ, mapZ, 255));
-                fill(color(mapZ, mapZ, 255));
-            }
-            */
             //water
-            if(mapZ > -16f && mapZ < -3f){
-                stroke(color(0, 0, 255));
-                fill(color(0, 0, 255));
+            if (mapZ > -16f && mapZ < 4.6f) {
+                stroke(color(mapZ, mapZ, mapZ));
+                fill(color(mapZ, mapZ, mapZ));
             }
-            
+
             //ground
-            if(mapZ > -3f && mapZ < 20f){
-                stroke(color((mapZ*3), (mapZ*3), (mapZ*3)));
-                fill(color((mapZ*3), (mapZ*3), (mapZ*3)));
+            if (mapZ > 4.6f && mapZ < 23f) {
+                stroke(color(mapZ, mapZ, mapZ));
+                fill(color(mapZ, mapZ, mapZ));
             }
-            
+
             //top of buildings
-            if(mapZ > 20f && mapZ < 215f){
-                stroke(color(mapZ, mapZ, 0));
-                fill(color(mapZ, mapZ, 0));
+            if (mapZ > 23f && mapZ < 215f) {
+                stroke(color(mapZ, mapZ, mapZ));
+                fill(color(mapZ, mapZ, mapZ));
             }
-            //water
-            else{
-            stroke(color(50, 50, 50));
-            fill(color(50, 50, 50));
-            }
-            
-            ellipse(mapX, mapY, 13, 13);            //create ellipse at points of mapped xy
+
+            rect(mapX, mapY, 13f, 13f);            //create ellipse at points of mapped xy
         } catch (Exception e) {
             logger.info(e);
+        }
+    }
+
+    //Method to create ellipses from startMap
+    public void createPoints() {
+        for (int i = 0; i < results.size(); i++) {
+            startMap(results.get(i).x, results.get(i).y, results.get(i).z);
         }
     }
 
     @Override
     public void draw() {
         //Draw all points
-        for (int i = 0; i < results.size(); i++) {
-            startMap(results.get(i).x, results.get(i).y, results.get(i).z);
+        if (!paused) {
+            createPoints();
+        }
+        
+        startButton.draw();
+        stopButton.draw();
+    }
+
+    public void handleStartButtonEvents(GButton button, GEvent event) {
+        // If we have more than one button then this is how we
+        // check which one was cllicked.
+        if (button == startButton && event == GEvent.CLICKED) {
+            paused = false;
+            JOptionPane.showMessageDialog(null, "Resuming Program....", "Resumed", JOptionPane.INFORMATION_MESSAGE);
+            this.resume();
+            logger.info("Resuming....");
+        }
+    }
+
+    public void handleStopButtonEvents(GButton button, GEvent event) {
+        // If we have more than one button then this is how we
+        // check which one was cllicked.
+        if (button == stopButton && event == GEvent.CLICKED) {
+            paused = true;
+            JOptionPane.showMessageDialog(null, "Pausing Program....", "Paused", JOptionPane.INFORMATION_MESSAGE);
+            this.pause();
+            logger.info("Paused....");
         }
     }
 

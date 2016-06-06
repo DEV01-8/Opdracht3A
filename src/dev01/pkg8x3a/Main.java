6d63a4073a188981d4b5b2cbb514bbafbc36b068
Johan Bos <bosbosjohan@gmail.com>
2016-06-06 16:45:44 +0200

    water werkt
    - Waarde waterline bepalen
    - Start/Stop/Reset knop

--- src/dev01/pkg8x3a/Main.java
+++ src/dev01/pkg8x3a/Main.java
@@ -18,27 +18,29 @@ import processing.core.PVector;
  */
 public class Main extends PApplet {
 
-    public static ArrayList<PVector> results = new ArrayList();
+    private static ArrayList<PVector> results = new ArrayList();
+    private static ArrayList<PVector> mappings = new ArrayList();
+
     private final float startX = 92799f;
     private final float startY = 436964f;
     private final float minX = startX - 1000f;
     private final float maxX = startX + 1000f;
     private final float minY = startY - 1000f;
     private final float maxY = startY + 1000f;
-    final static Logger logger = Logger.getLogger(Main.class);
+    final Logger logger = Logger.getLogger(Main.class);
     float waterline = -16f;
 
     @Override
     public void setup() {
         background(255);
+        frameRate(1);
         surface.setTitle("Hoogtebestand Rotterdam Oost");
-
         results = parseCSV.read();  //Get all items from parseCSV
     }
 
     @Override
     public void settings() {
-        size(680, 680);
+        size(680, 680, JAVA2D);
 
     }
 
@@ -49,43 +51,44 @@ public class Main extends PApplet {
 
     //Method to map xyz coordinates
     private void startMap(float x, float y, float z) {
-        try {
-            float minZ = parseCSV.minZ;     //min value of Z ~ -16
-            float maxZ = parseCSV.maxZ;     //max value of z ~ 215
+        float minZ = parseCSV.minZ;     //min value of Z ~ -16
+        float maxZ = parseCSV.maxZ;     //max value of z ~ 215
 
-            float mapX = map(x, minX, maxX, 0, width);   //map x
-            float mapY = map(y, maxY, minY, 0, height);  //map y
-            float mapZ = map(z, minZ, maxZ, 0, 216);     //map z
+        float mapX = map(x, minX, maxX, 0, width);   //map x
+        float mapY = map(y, maxY, minY, 0, height);  //map y
+        float mapZ = map(z, minZ, maxZ, 0, 216);     //map z
 
-            //ground
-            if (mapZ > 4.6f && mapZ < 23f) {
-                stroke(color(mapZ, mapZ, mapZ));
-                fill(color(mapZ, mapZ, mapZ));
-            }
+        PVector mappedVector = new PVector(mapX, mapY, mapZ);
+        mappings.add(mappedVector);
+    }
 
-            //top of buildings
-            if (mapZ > 23f && mapZ < 215f) {
-                stroke(color(mapZ, mapZ, mapZ));
-                fill(color(mapZ, mapZ, mapZ));
-            }
-
-            //water
-            if (waterline > mapZ) {
-                stroke(color(0, 0, 255));
-                fill(color(0, 0, 255));
-            }
-
-            rect(mapX, mapY, 13f, 13f);            //create ellipse at points of mapped xy
-
-        } catch (Exception e) {
-            logger.info(e);
+    private void createMap(float mapX, float mapY, float mapZ) {
+        //ground
+        if (mapZ > 4.6f && mapZ < 23f) {
+            stroke(color(mapZ, mapZ, mapZ));
+            fill(color(mapZ, mapZ, mapZ));
         }
+
+        //top of buildings
+        if (mapZ > 23f && mapZ < 215f) {
+            stroke(color(mapZ, mapZ, mapZ));
+            fill(color(mapZ, mapZ, mapZ));
+        }
+
+        //water
+        if (waterline > mapZ) {
+            stroke(color(0, 0, 255));
+            fill(color(0, 0, 255));
+        }
+
+        rect(mapX, mapY, 13f, 13f);            //create ellipse at points of mapped xy
+
     }
 
     //Method to create ellipses from startMap
-    public void createPoints() {
+    private void createPoints() {
         for (int i = 0; i < results.size(); i++) {
-            startMap(results.get(i).x, results.get(i).y, results.get(i).z);
+            createMap(results.get(i).x, results.get(i).y, results.get(i).z);
         }
     }
 
@@ -93,12 +96,6 @@ public class Main extends PApplet {
     public void draw() {
         //Draw all points
         createPoints();
-    }
-    
-    @Override
-    public void mouseClicked(){
-        waterline = waterline + 5f;
-        //redraw();
-        createPoints();
+        waterline = waterline + 1.00f;
     }
 }

cdd28423c4b8e9aa2be2b5c87752b4f57b043154
Johan Bos <bosbosjohan@gmail.com>
2016-06-06 20:12:47 +0200

    Start en Stop buttons toegevoegd

--- src/dev01/pkg8x3a/Main.java
+++ src/dev01/pkg8x3a/Main.java
@@ -5,13 +5,13 @@
  */
 package dev01.pkg8x3a;
 
+import g4p_controls.GButton;
+import g4p_controls.GEvent;
 import java.util.ArrayList;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import processing.core.PApplet;
 import static processing.core.PApplet.map;
-import processing.core.PConstants;
-import processing.core.PGraphics;
 import processing.core.PVector;
 
 /**
@@ -29,16 +29,29 @@ public class Main extends PApplet {
     private final float maxX = startX + 1000f;
     private final float minY = startY - 1000f;
     private final float maxY = startY + 1000f;
+
     final Logger logger = Logger.getLogger(Main.class);
     float waterline = -16f;
+    boolean first;
+    boolean loop = true;
+    GButton startButton;
+    GButton stopButton;
 
     @Override
     public void setup() {
         background(255);
         frameRate(1);
         surface.setTitle("Hoogtebestand Rotterdam Oost");
+
         results = parseCSV.read();  //Get all items from parseCSV
         startMap();
+
+        startButton = new GButton(this, 30, 30, 200, 40, "Start");
+        stopButton = new GButton(this, 250, 30, 200, 40, "Stop");
+
+        startButton.addEventHandler(this, "handleStartButtonEvent");
+        stopButton.addEventHandler(this, "handleStopButtonEvent");
+
     }
 
     @Override
@@ -56,7 +69,7 @@ public class Main extends PApplet {
     private void startMap() {
         float minZ = parseCSV.minZ;     //min value of Z ~ -16
         float maxZ = parseCSV.maxZ;     //max value of z ~ 215
-
+        
         for (int i = 0; i < results.size(); i++) {
             float mapX = map(results.get(i).x, minX, maxX, 0, width);   //map x
             float mapY = map(results.get(i).y, maxY, minY, 0, height);  //map y
@@ -65,11 +78,22 @@ public class Main extends PApplet {
             PVector mappedVector = new PVector(mapX, mapY, mapZ);
             mappings.add(mappedVector);
         }
-        
+
     }
 
     private void createMap(float mapX, float mapY, float mapZ) {
         //ground
+        if (mapZ > 4.6f && mapZ < 23f) {
+            stroke(color(mapZ, mapZ, mapZ));
+            fill(color(mapZ, mapZ, mapZ));
+        }
+
+        //top of buildings
+        if (mapZ > 23f && mapZ < 215f) {
+            stroke(color(mapZ, mapZ, mapZ));
+            fill(color(mapZ, mapZ, mapZ));
+        }
+
         if (mapZ > 4.6f && mapZ < 23f) {
             stroke(color(mapZ, mapZ, mapZ));
             fill(color(mapZ, mapZ, mapZ));
@@ -103,5 +127,20 @@ public class Main extends PApplet {
         //Draw all points
         createPoints();
         waterline = waterline + 0.25f;
+
+        startButton.draw();
+        stopButton.draw();
+    }
+
+    public void handleStartButtonEvent(GButton button, GEvent event) {
+        if (button == startButton && event == GEvent.CLICKED) {
+            logger.info("StartButton is pressed!");
+        }
+    }
+
+    public void handleStopButtonEvent(GButton button, GEvent event) {
+        if (button == stopButton && event == GEvent.CLICKED) {
+            logger.info("StopButton is pressed!");
+        }
     }
 }
